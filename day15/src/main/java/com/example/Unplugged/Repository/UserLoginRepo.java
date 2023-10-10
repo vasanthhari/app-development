145411acb46cae07542937bca59c2a3c4c3f6954
vasanthhari <110531489+vasanthhari@users.noreply.github.com>
2023-10-10 11:09:04 +0530

    Delete day15 directory

--- day15/src/main/java/com/example/Unplugged/Repository/UserLoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Unplugged.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Unplugged.Entity.UserLogin;
-
-public interface UserLoginRepo extends JpaRepository<UserLogin,Integer> {
-
-}
